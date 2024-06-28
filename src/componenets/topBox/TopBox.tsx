11d218e7a53cfaaf23ed435597412b2c8cbc59c5
Hazem1234321 <hazemmohsen188@gmail.com>
2024-06-28 22:12:38 +0300

    grid system

---
+++ src/componenets/topBox/TopBox.tsx
@@ -0,0 +1,8 @@
+import "./topbox.scss";
+
+const TopBox = () => {
+  return (
+    <div>TopBox</div>
+  )
+}
+export default TopBox
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -2,7 +2,18 @@
 import "./home.scss"
 const home = () => {
   return (
-    <div className="home">home</div>
+    <div className="home">
+      <div className="box box1">box1</div>
+      <div className="box box2">box2</div>
+      <div className="box box3">box3</div>
+      <div className="box box4">box4</div>
+      <div className="box box5">box5</div>
+      <div className="box box6">box6</div>
+      <div className="box box7">box7</div>
+      <div className="box box8">box8</div>
+      <div className="box box9">box9</div>
+
+    </div>
   )
 }
 export default home
