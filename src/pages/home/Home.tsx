308b8828fa713a09096d53fe1f21c30dac0f3557
Hazem1234321 <hazemmohsen188@gmail.com>
2024-06-28 22:40:27 +0300

    home pages updates

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,9 +1,10 @@
 
+import TopBox from "../../componenets/topBox/TopBox"
 import "./home.scss"
 const home = () => {
   return (
     <div className="home">
-      <div className="box box1">box1</div>
+      <div className="box box1"><TopBox/></div>
       <div className="box box2">box2</div>
       <div className="box box3">box3</div>
       <div className="box box4">box4</div>
