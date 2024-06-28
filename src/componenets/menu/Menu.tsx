a103b857a5c4f2b3210990d44da3ebb86faa0fdd
Hazem1234321 <hazemmohsen188@gmail.com>
2024-06-28 21:35:25 +0300

    data and menu

--- src/componenets/menu/Menu.tsx
+++ src/componenets/menu/Menu.tsx
@@ -1,7 +1,34 @@
+import { Link } from "react-router-dom"
 import "./menu.scss"
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className="menu">
+      <div className="item">
+        <span className="title">MAIN</span>
+        <Link to="/" className="ListItem">
+          <img src="/home.svg" alt="home icon" />
+          <span className="ListItemTitle">Home</span>
+        </Link>
+        <Link to="profile" className="ListItem">
+          <img src="/profile.svg" alt="profile icon" />
+          <span className="ListItemTitle">Profile</span>
+        </Link>
+      </div>
+
+      <div className="item">
+        <span className="title">MAIN</span>
+        <Link to="/" className="ListItem">
+          <img src="/home.svg" alt="home icon" />
+          <span className="ListItemTitle">Home</span>
+        </Link>
+        <Link to="profile" className="ListItem">
+          <img src="/profile.svg" alt="profile icon" />
+          <span className="ListItemTitle">Profile</span>
+        </Link>
+      </div>
+     
+    </div>
+
   )
 }
 export default Menu
