bb4bfccf2c1f8865c88b77d472c2c499dc11e93e
Hazem1234321 <hazemmohsen188@gmail.com>
2024-06-29 07:11:05 +0300

    making the box charts

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,19 +1,20 @@
 
+import Chartbox from "../../componenets/chartbox/Chartbox"
 import TopBox from "../../componenets/topBox/TopBox"
+import { chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from "../../data"
 import "./home.scss"
 const home = () => {
   return (
     <div className="home">
       <div className="box box1"><TopBox/></div>
-      <div className="box box2">box2</div>
-      <div className="box box3">box3</div>
-      <div className="box box4">box4</div>
-      <div className="box box5">box5</div>
-      <div className="box box6">box6</div>
+      <div className="box box2"><Chartbox {...chartBoxUser}/></div>
+      <div className="box box3"><Chartbox {...chartBoxProduct}/></div>
+      <div className="box box4">box 4</div>
+      <div className="box box5"><Chartbox {...chartBoxRevenue}/></div>
+      <div className="box box6"><Chartbox {...chartBoxConversion}/></div>
       <div className="box box7">box7</div>
       <div className="box box8">box8</div>
       <div className="box box9">box9</div>
-
     </div>
   )
 }
