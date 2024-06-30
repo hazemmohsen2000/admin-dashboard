d4c927e6dfd05c2efd99abdf8aad98e9c364ee12
Hazem1234321 <hazemmohsen188@gmail.com>
2024-06-30 07:50:59 +0300

    adding area chart

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 
 import BarChartBox from "../../componenets/barChartBox/BarChartBox"
+import BigChartBox from "../../componenets/bigChartBox/BigChartBox"
 import Chartbox from "../../componenets/chartbox/Chartbox"
 import PieChartBox from "../../componenets/pieChartBox/PieChartBox"
 import TopBox from "../../componenets/topBox/TopBox"
@@ -14,7 +15,7 @@ const home = () => {
       <div className="box box4"><PieChartBox/></div>
       <div className="box box5"><Chartbox {...chartBoxRevenue}/></div>
       <div className="box box6"><Chartbox {...chartBoxConversion}/></div>
-      <div className="box box7">box7</div>
+      <div className="box box7"><BigChartBox/></div>
       <div className="box box8"><BarChartBox {...barChartBoxVisit}/></div>
       <div className="box box9"><BarChartBox {...barChartBoxRevenue}/></div>
     </div>
