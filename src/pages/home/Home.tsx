654c9c0a38771e2806b650897cddfaf3528b1b01
Hazem1234321 <hazemmohsen188@gmail.com>
2024-06-30 07:35:24 +0300

    creating pie chart

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,6 +1,7 @@
 
-import BarChartBox from "../../componenets/barChartBox/barChartBox"
+import BarChartBox from "../../componenets/barChartBox/BarChartBox"
 import Chartbox from "../../componenets/chartbox/Chartbox"
+import PieChartBox from "../../componenets/pieChartBox/PieChartBox"
 import TopBox from "../../componenets/topBox/TopBox"
 import { barChartBoxRevenue, barChartBoxVisit, chartBoxConversion, chartBoxProduct, chartBoxRevenue, chartBoxUser } from "../../data"
 import "./home.scss"
@@ -10,7 +11,7 @@ const home = () => {
       <div className="box box1"><TopBox/></div>
       <div className="box box2"><Chartbox {...chartBoxUser}/></div>
       <div className="box box3"><Chartbox {...chartBoxProduct}/></div>
-      <div className="box box4">box 4</div>
+      <div className="box box4"><PieChartBox/></div>
       <div className="box box5"><Chartbox {...chartBoxRevenue}/></div>
       <div className="box box6"><Chartbox {...chartBoxConversion}/></div>
       <div className="box box7">box7</div>
