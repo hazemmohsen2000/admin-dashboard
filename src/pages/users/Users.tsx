670771ba17530553ef87f2986e83a01b841d65e0
Hazem1234321 <hazemmohsen188@gmail.com>
2024-06-30 21:28:51 +0300

    installing mui

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -2,7 +2,12 @@
 import "./users.scss"
 const Users = () => {
   return (
-    <div className="users">Users</div>
+    <div className="users">
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add new User</button>
+      </div>
+    </div>
   )
 }
 export default Users
