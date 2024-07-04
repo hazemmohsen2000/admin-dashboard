0d642ad4e48f0fbbe9278202deaa3a731199829a
hazemmohsen2000 <hazemmohsen188@gmail.com>
2024-07-04 04:44:57 +0300

    making products page

--- src/pages/products/Products.tsx
+++ src/pages/products/Products.tsx
@@ -1,7 +1,68 @@
+import { useState } from "react"
 import "./products.scss"
+import DataTable from "../../componenets/dataTable/DataTable"
+import Add from "../../componenets/add/Add"
+import { products } from "../../data";
+const columns: GridColDef[] = [
+  { field: "id", headerName: "ID", width: 90 },
+  {
+    field: "img",
+    headerName: "Image",
+    width: 100,
+    renderCell: (params) => {
+      return <img src={params.row.img || "/noavatar.png"} alt="" />;
+    },
+  },
+  {
+    field: "title",
+    type: "string",
+    headerName: "Title",
+    width: 250,
+  },
+  {
+    field: "color",
+    type: "string",
+    headerName: "Color",
+    width: 150,
+  },
+  {
+    field: "price",
+    type: "string",
+    headerName: "Price",
+    width: 200,
+  },
+  {
+    field: "producer",
+    headerName: "Producer",
+    type: "string",
+    width: 200,
+  },
+  {
+    field: "createdAt",
+    headerName: "Created At",
+    width: 200,
+    type: "string",
+  },
+  {
+    field: "inStock",
+    headerName: "In Stock",
+    width: 150,
+    type: "boolean",
+  },
+];
 const Products = () => {
-  return (
-    <div className="products">Products</div>
-  )
+  
+  const [open, setOpen] = useState(false);
+    return (
+    <div className="products">
+      <div className="info">
+        <h1>Users</h1>
+        <button onClick={()=>setOpen(true)}>Add new Products</button>
+      </div>
+      <DataTable  slug="Products" columns ={columns} row = {products}/>
+      {open && <Add slug = "Products" columns={columns} setOpen = {setOpen}/>}
+    </div>
+  
+)
 }
 export default Products
